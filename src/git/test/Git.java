f47ec0f84453f055140aa4e2a63317991b410701
southben <723158053@qq.com>
2020-06-09 10:13:38 +0800

    我的git项目

--- src/git/test/Git.java
+++ src/git/test/Git.java
@@ -2,6 +2,6 @@ package git.test;
 
 public class Git {
     public static void main(String[] args) {
-        System.out.println("git使用测试。。。");
+        System.out.println("git使用测试。。。  ");
     }
 }
